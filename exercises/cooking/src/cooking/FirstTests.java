4d5c113acea29aa0199661475f37cd6e8356eb8d
B T Forsythe <brian@btforsythe.io>
2017-10-02 16:09:35 -0400

    initial commit

---
+++ exercises/cooking/src/cooking/FirstTests.java
@@ -0,0 +1,16 @@
+package cooking;
+
+import org.junit.Test;
+
+/**
+ * We are doing something unusual here. Usually the tests for a class would be
+ * found in a corresponding *Test class. Here we are combining unit tests so
+ * that we can make them pass in a prescribed order.
+ */
+public class FirstTests {
+
+	@Test
+	public void shouldFoo() {
+
+	}
+}
